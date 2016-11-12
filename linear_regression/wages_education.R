68be5e5cc38dae7d4a1f9473e49cadf3a11eb498
Abhinav <abhinav@cloudxlab.com>
2016-11-12 20:36:37 +0530

    Added code for plotting line of fit in wages_education.R

--- linear_regression/wages_education.R
+++ linear_regression/wages_education.R
@@ -5,9 +5,13 @@ fit <- lm(Wages ~ Education)
 
 #Find more about fit
 attributes(fit)
+summary(fit)
 
 #predict
 
 new_data = data.frame(Education=14)
 
 predict(fit, new_data)
+
+plot(Education, Wages)
+abline(fit)

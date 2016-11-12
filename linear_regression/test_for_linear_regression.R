f220af94e89bc4f3ac5ea803dc20776617f021dc
Abhinav <abhinav@cloudxlab.com>
2016-11-12 20:17:59 +0530

    Moved wages plotting to y axis in test_for_linear_regression.R

--- linear_regression/test_for_linear_regression.R
+++ linear_regression/test_for_linear_regression.R
@@ -1,3 +1,3 @@
 my_data = read.table("linear_regression/wages_education.csv", header=TRUE, sep=",")
-plot(my_data$Wages, my_data$Education)
-cor(my_data$Wages, my_data$Education)
+plot(my_data$Education, my_data$Wages)
+cor(my_data$Education, my_data$Wages)

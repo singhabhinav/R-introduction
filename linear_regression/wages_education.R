dc57b5506addb72f8da01a8a7bb77cef862d91d1
Abhinav <abhinav@cloudxlab.com>
2016-11-12 20:31:54 +0530

    Added code for linear regression for wages and education

---
+++ linear_regression/wages_education.R
@@ -0,0 +1,13 @@
+my_data = read.table("linear_regression/wages_education.csv", header=TRUE, sep=",")
+attach(my_data)
+
+fit <- lm(Wages ~ Education)
+
+#Find more about fit
+attributes(fit)
+
+#predict
+
+new_data = data.frame(Education=14)
+
+predict(fit, new_data)

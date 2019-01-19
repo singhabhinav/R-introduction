3a2da67137cb406f6159f2d9a928d2d23a6317fc
abhinav <abhinav.foss@gmail.com>
2019-01-19 13:27:17 +0530

    Improved scraping wikipedia code

--- scraping/scraping_wikipedia.py
+++ scraping/scraping_wikipedia.py
@@ -1,3 +1,9 @@
+
+# coding: utf-8
+
+# In[3]:
+
+
 # To make HTTP request
 import requests
 
@@ -13,33 +19,36 @@ headers = {'User-Agent': user_agent}
 # Get URL content
 page = requests.get(url, headers)
 
-print(page.content)
+#print(page.content)
 
 from bs4 import BeautifulSoup
 soup = BeautifulSoup(page.content, 'html.parser')
 
-print(soup.prettify())
+#print(soup.prettify())
 
-print(soup.title)
+#print(soup.title)
 
 # Just for practice
 # Get all hyperlinks in the page
 all_links = soup.find_all("a")
-print(all_links)
-for link in all_links:
-    print(link.get('href'))
+#print(all_links)
+#for link in all_links:
+ #   print(link.get('href'))
 
 # Get all tables
 all_tables = soup.find_all('table')
-print(all_tables)
+#print(all_tables)
 
 
 # Select the right table in which data is there
 right_table = soup.find('table', class_='wikitable sortable plainrowheaders')
+
 print(right_table)
 
 
-# Generate lists to store scraped data
+# In[69]:
+
+
 A = [] # For Number
 B = [] # For State/UT
 C = [] # For Administrative capitals
@@ -51,22 +60,13 @@ G = [] # The Former capital
 for row in right_table.findAll("tr"):
     cells = row.findAll('td')
     states = row.findAll('th') #To store second column data
-    if len(cells) == 6: # Only extract table body not heading
-        A.append(cells[0].find(text=True))
-        B.append(states[0].find(text=True))
-        C.append(cells[1].find(text=True))
-        D.append(cells[2].find(text=True))
-        E.append(cells[3].find(text=True))
-        F.append(cells[4].find(text=True))
-        G.append(cells[5].find(text=True))
+    if len(cells) == 5:
+        A.append(states[0].find(text=True).rstrip())
+        B.append(states[1].find(text=True).rstrip())
+        C.append(cells[0].find(text=True).rstrip())
+        D.append(cells[1].find(text=True).rstrip())
+        E.append(cells[2].find(text=True).rstrip())
+        F.append(cells[3].find(text=True).rstrip())
+        G.append(cells[4].find(text=True).rstrip())
+print(A)
 
-import pandas as pd
-df = pd.DataFrame(A,columns=['Number'])
-df['State/UT'] = B
-df['Admin_Capital'] = C
-df['Legislative_Capital'] = D
-df['Judiciary_Capital'] = E
-df['Year_Capital'] = F
-df['Former_Capital'] = G
-print(df)
-df.to_csv("capitals.csv", sep='\t', encoding='utf-8')
